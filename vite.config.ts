9a28efa5dd187a0c21aa5ba2654c02ab7831b62d
vishalnimbalkar55 <vishalnimbalkar55@gmail.com>
2024-09-23 20:25:52 +0530

    initial folder structure

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css:{
+    preprocessorOptions:{
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  },
+  server:{
+    port:3000
+  },
+  preview:{
+    port:3000
+  },
+})
